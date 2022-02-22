551b3027dd75bc415aca85ac159c3d8b45a6c49e
Proulx-S <sebastien.proulx2@mail.mcgill.ca>
2022-02-22 15:04:27 -0500

    add phase-space plot

--- run_vanLoonSims_example.m
+++ run_vanLoonSims_example.m
@@ -16,23 +16,61 @@ p.startingA = [0 0];
 
 
 %% Params ranges
-            curP = p;
+curP = p;
 %             curP.(char(param)) = pR.(char(param))(i);
-            curP.I      = [curP.I     curP.I    ];
-            curP.gamma  = [curP.gamma curP.gamma];
-            curP.sigma  = [curP.sigma curP.sigma];
-            curP.alpha  = [curP.alpha curP.alpha];
-            curP.tau    = [curP.tau   curP.tau  ];
-            
-            tic
-            [times,Var] = vanLoonSim(curP);
-            plot_vanLoonSims(length(times)-1,1,times,Var)
-            toc
+curP.I      = [curP.I     curP.I    ];
+curP.gamma  = [curP.gamma curP.gamma];
+curP.sigma  = [curP.sigma curP.sigma];
+curP.alpha  = [curP.alpha curP.alpha];
+curP.tau    = [curP.tau   curP.tau  ];
 
+tic
+[times,Var] = vanLoonSim(curP);
+plot_vanLoonSims(length(times)-1,1,times,Var)
+toc
 
-
-
-
+if 0
+    x1 = Var(:,1);
+    x2 = Var(:,2);
+    d = 500;
+    gridx = linspace(min([x1; x2]),max([x1; x2; 1]),d);
+    gridi = 1:1:d;
+    [x1,x2] = meshgrid(gridx, gridx);
+    [i1,i2] = meshgrid(gridi, gridi);
+    x1 = x1(:);
+    x2 = x2(:);
+    xi = [x1 x2];
+    i1 = i1(:);
+    i2 = i2(:);
+    i = [i1 i2];
+    v1 = Var(:,1);
+    v2 = Var(:,2);
+    v = [v1 v2];
+    [v,xi] = ksdensity(v,xi);
+    v = full( sparse(i(:,1), i(:,2), v, d, d) );
+    i1 = full( sparse(i(:,1), i(:,2), i(:,1), d, d) );
+    i2 = full( sparse(i(:,1), i(:,2), i(:,2), d, d) );
+    i1 = i1(:,1);
+    i2 = i2(1,:)';
+    x1 = full( sparse(i(:,1), i(:,2), xi(:,1), d, d) );
+    x2 = full( sparse(i(:,1), i(:,2), xi(:,2), d, d) );
+    x1 = x1(:,1);
+    x2 = x2(1,:)';
+    save(mfilename,'x1','x2','v')
+end
+load(mfilename,'x1','x2','v')
+figure('WindowStyle','docked')
+imagesc(x1,x2,v)
+ax = gca;
+ax.YDir = 'normal';
+ax.PlotBoxAspectRatio = [1 1 1];
+ax.DataAspectRatio  = [1 1 1];
+xlim([0.4 1])
+ylim([0.4 1])
+color = colormap(gray);
+color = color(end:-1:1,:);
+colormap(ax,color)
+grid on
 
 
 
