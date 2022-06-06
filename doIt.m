4ba445880bec1bc37dc9ba5ced927a9c5d867778
Proulx-S <sebastien.proulx2@mail.mcgill.ca>
2022-06-06 15:25:46 -0400

    Add a step by step demo of the rivalry dynamics
    Start with a system that has no inputs, no mutual inhibition and no adaptation, then successively add these ingredients.

--- doIt.m
+++ doIt.m
@@ -7,9 +7,14 @@ visualize_vanLoonSims
 
 
 
-
 %% Extra Experimental Stuff
-run_vanLoonSims_example
+% a simple example of alternating states
+run_vanLoonSims_example;
+% another example, starting with a system with no input, no mutual inhibition and no adaptation, to which with then successively add (1) inputs, (2) mutual inhibition and (3) adaptation
+run_vanLoonSims_example_stepByStep;
+
+
+
 
 run_vanLoonSims_symetric_gammaOnly
 analyze_vanLoonSims_gammaOnly
---
+++ run_vanLoonSims_example_stepByStep.m
@@ -0,0 +1,81 @@
+clear all
+close all
+
+%% Params default
+p.I       = 1; % 1
+p.gamma   = 3; % 3
+p.sigma   = 0.010; %0.003
+p.alpha   = 4; % 4
+p.tau     = 125;
+p.sigmoidParam(1) = 0.5;
+p.sigmoidParam(2) = 18;
+p.sigmoidParam(3) = 1;
+p.endTime = [20 20 20 175];
+
+p.startingI = [0 0];
+p.startingA = [0 0];
+
+
+curP = p;
+curP.I      = [curP.I     curP.I    ];
+curP.gamma  = [curP.gamma curP.gamma];
+curP.sigma  = [curP.sigma curP.sigma];
+curP.alpha  = [curP.alpha curP.alpha];
+curP.tau    = [curP.tau   curP.tau  ];
+
+i = 0;
+%% Start with everything off
+i = i+1; curP.endTime = p.endTime(i);
+curP.startingI = [0 0];
+curP.startingA = [0 0];
+curP.I      = [0 0];
+curP.gamma  = [0 0];
+curP.alpha  = [0 0];
+
+[curTimes,curVar] = vanLoonSim(curP);
+times = curTimes;
+Var = curVar;
+
+
+%% Turn on inputs
+i = i+1; curP.endTime = p.endTime(i);
+curP.I         = [1 1];
+curP.startingI = Var(end,[1 2]);
+curP.startingA = Var(end,[3 4]);
+
+[curTimes,curVar] = vanLoonSim(curP);
+times = cat(1,times,curTimes+times(end));
+Var = cat(1,Var,curVar);
+
+
+%% Turn on inhibition
+i = i+1; curP.endTime = p.endTime(i);
+curP.gamma     = [3 3];
+curP.startingI = Var(end,[1 2]);
+curP.startingA = Var(end,[3 4]);
+
+[curTimes,curVar] = vanLoonSim(curP);
+times = cat(1,times,curTimes+times(end));
+Var = cat(1,Var,curVar);
+
+
+%% Turn on adaptation
+i = i+1; curP.endTime = p.endTime(i);
+curP.alpha     = [4 4];
+curP.startingI = Var(end,[1 2]);
+curP.startingA = Var(end,[3 4]);
+
+[curTimes,curVar] = vanLoonSim(curP);
+times = cat(1,times,curTimes+times(end));
+Var = cat(1,Var,curVar);
+
+
+%% Plot
+plot_vanLoonSims(length(times)-1,1,times,Var)
+f = gcf;
+delete(f.Children(1))
+for i = 1:length(p.endTime)
+    line(sum(p.endTime(1:i)).*[1 1],ylim);
+end
+saveas(f,[mfilename '.fig']);
+saveas(f,[mfilename '.svg']);
